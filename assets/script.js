7757797138b1f15c548cc828fcc7c0fa3d93e307
hilbug <65197724+hilbug@users.noreply.github.com>
2020-07-11 17:18:25 -0400

    javascript notes

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// variables for page elements
+// time... let timeEl = document.querySelector("#time");
+// section H1 and Intro
+// section Question and answers
+// start quiz button
+// score
+// user initials
+
+// Need object for Question and answers
+
+//Listening events
+// click start quiz
+// click an answer choice
