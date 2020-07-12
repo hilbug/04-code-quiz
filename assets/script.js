cc164a04ddc1188db84260433eaaae6f8079c582
hilbug <65197724+hilbug@users.noreply.github.com>
2020-07-11 23:32:29 -0400

    add pseudocode to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,49 @@
 // variables for page elements
-// time... let timeEl = document.querySelector("#time");
-// section H1 and Intro
-// section Question and answers
-// start quiz button
-// score
-// user initials
+    // time... let timeEl = document.querySelector("#time");
+    // score
+    // user initials
+    // sections
+        // section intro
+        // section questions
+        // section final
+        // section highscores
+        // div yaynay
+    // buttons
+        // start 
+        // answer1
+        // answer2
+        // answer3
+        // answer4
+        // submit-score
+        // goback
+        // clearscores
+        // view-scores
+    // Object for Question, answer, true/false
 
-// Need object for Question and answers
-
-//Listening events
-// click start quiz
-// click an answer choice
+//Events
+    // click start quiz
+        // moves to question 1
+            // set question
+            // set answers 1 through 4
+        // start timer
+    // click an answer choice
+        // if correct, show yay
+        // if incorrect, show nay
+        // if incorrect, deduct 10 from time
+        // set to next question
+            // set question
+            // set answers 1 through 4
+            // loop through to last question
+    // After last question OR time reaches 0
+        // show "final" section
+        // store score (time) to local storage
+    // click submit for initials
+        // show high scores page
+        // add latest entry to high scores/object?
+        // High scores should display as an ordered list descending
+    // click go back button
+        // go back to intro section
+    // click clear scores button
+        // clear the high scores array/object
+    // click view high scores
+        // go to high scores section
